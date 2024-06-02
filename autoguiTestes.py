d932e62bc540f19d6f6e677aeedbfb882409dcda
André Judá <judatrainer@gmail.com>
2024-06-02 09:26:51 -0300

    v1

---
+++ autoguiTestes.py
@@ -0,0 +1,9 @@
+import pyautogui 
+
+pyautogui.click(x=1572, y=401, duration= 1)
+
+pyautogui.rightClick()
+
+pyautogui.click(x=1589, y=670, duration= 1)
+
+pyautogui.click(x=1542, y=672, duration= 1)

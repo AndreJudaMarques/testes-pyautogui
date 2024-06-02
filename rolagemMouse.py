29d3893c00c5dec13ba9ececa5e52c05d137ab3f
André Judá <judatrainer@gmail.com>
2024-06-02 10:23:13 -0300

    v1

---
+++ rolagemMouse.py
@@ -0,0 +1,13 @@
+import pyautogui
+from time import sleep
+
+#rolagem do mouse, botao do meio
+pyautogui.moveTo(x=1398, y=221, duration= 0.5)
+pyautogui.click()
+pyautogui.moveTo(x=1290, y=449, duration= 0.5)
+pyautogui.click()
+pyautogui.moveTo(x=1290, y=485, duration= 0.5)
+
+for i in range(10000):
+      pyautogui.scroll(-1500)
+      sleep(1)

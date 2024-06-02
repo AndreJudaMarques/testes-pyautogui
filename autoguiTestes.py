a98af6d6f2aaa47ae84ec8ec427c7f230085b7cc
André Judá <judatrainer@gmail.com>
2024-06-02 09:35:17 -0300

    v2

--- autoguiTestes.py
+++ autoguiTestes.py
@@ -1,9 +1,11 @@
 import pyautogui 
 
-pyautogui.click(x=1572, y=401, duration= 1)
+# clica e cria pasta 9 vezes
+for i in range(9):
+      pyautogui.click(x=1572, y=401, duration= 1)
 
-pyautogui.rightClick()
+      pyautogui.rightClick()
 
-pyautogui.click(x=1589, y=670, duration= 1)
+      pyautogui.click(x=1589, y=670, duration= 1)
 
-pyautogui.click(x=1542, y=672, duration= 1)
+      pyautogui.click(x=1542, y=672, duration= 1)

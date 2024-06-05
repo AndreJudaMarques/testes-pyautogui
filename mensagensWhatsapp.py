666c77a8aa0a5a5f45aa7bd49ae14d15377fd090
André Judá <judatrainer@gmail.com>
2024-06-05 12:53:20 -0300

    mensagens

--- mensagensWhatsapp.py
+++ mensagensWhatsapp.py
@@ -4,20 +4,24 @@ import webbrowser
 import pyautogui
 from time import sleep
 
-telefones = 
+telefones = 'xxxxxxx'
+
+#x guilherme
+#x wanda
+#x priscila
 
 sleep(1)
 
 for telefone in telefones:
       webbrowser.open_new_tab(f'https://wa.me/{telefone}')
       sleep(2)
-      pyautogui.press('tab')
+      pyautogui.press('tab')         
       sleep(0.5)
       pyautogui.press('tab')
       sleep(0.5)
       pyautogui.press('space')
-      sleep(1.5)
-      pyautogui.typewrite('Oi amigo, estou testando um novo sistema de mensagens automáticas, fica bravo nao S2', interval=0.1)
+      sleep(2)
+      pyautogui.typewrite('Ola, provavelmente eu ja comprei pizza com voces e hoje trabalho com automatizacao de mensagens, gostaria de enviar mensagens automaticas para seus clientes? como catálogos, promoçoes ou entao apenas lembrando a eles que sexta feira é um otimo dia para comprar pizza, se sim entre em contato comigo para eu te passar um programa que faz isso automatico para voce. Até mais. ', interval=0.05)
       sleep(1)
       pyautogui.press('tab')
       sleep(0.5)
